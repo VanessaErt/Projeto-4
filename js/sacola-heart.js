9c9e58e4812e0e0cd7a809e6f0fcb9f4d5acf647
VanessaErt <vanessaertsoares@gmail.com>
2024-10-14 09:10:57 -0300

    Projeto4

---
+++ js/sacola-heart.js
@@ -0,0 +1,9 @@
+function adicionarAoCarrinho() {
+    // Aqui você pode adicionar lógica para adicionar o produto à sacola
+    alert('Produto adicionado à sacola!');
+}
+
+function adicionarAosFavoritos() {
+    // Aqui você pode adicionar lógica para adicionar o produto aos favoritos
+    alert('Produto adicionado aos favoritos!');
+}
